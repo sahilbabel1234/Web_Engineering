2c948687739564e5a4218631b91e5c24533c5935
Sahil Babel <sahilbabel951@gmail.com>
2021-02-02 21:03:20 +0100

    initial commit

--- InternshipTutor/src/main/webapp/js/main.js
+++ InternshipTutor/src/main/webapp/js/main.js
@@ -5,3 +5,35 @@
   });
 
 })(jQuery);
+
+
+(function($) {
+  "use strict";
+  
+  // Porfolio isotope and filter
+  $(window).on('load', function() {
+    var portfolioIsotope = $('.portfolio-container').isotope({
+      itemSelector: '.portfolio-item'
+    });
+    $('#portfolio-flters li').on('click', function() {
+      $("#portfolio-flters li").removeClass('filter-active');
+      $(this).addClass('filter-active');
+
+      portfolioIsotope.isotope({
+        filter: $(this).data('filter')
+      });
+      aos_init();
+    });
+  });
+// Init AOS
+  function aos_init() {
+    AOS.init({
+      duration: 1000,
+      once: true
+    });
+  }
+  $(window).on('load', function() {
+    aos_init();
+  });
+
+})(jQuery);
